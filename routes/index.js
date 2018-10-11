580b6dc17c0670b4e164b54038d69c6e52e21390
Chris Ridgeway <chris.ridgeway01@gmail.com>
2018-10-11 13:38:36 +0000

    removed unneeded stuff for API template version (this branch)

--- routes/index.js
+++ routes/index.js
@@ -6,18 +6,5 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
-router.get('/add-to-cart/:id', function(req, res, next){
-  var productId = req.params.id;
-  var cart = new Cart(req.session.cart ? req.session.cart : {});
-
-  Product.findById(productId)
-      .then(function(err, product){
-          if(err){
-            return res.redirect('/');
-          }
-          cart.add(product, product.id);
-          req.session.cart = cart;
-  })
-});
 
 module.exports = router;
